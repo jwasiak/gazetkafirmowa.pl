05b6fdae2611a7e8d7724fad7cad80ede074fd83
Jarosław Wasiak <jaroslaw@wasiak.biz>
2021-04-06 09:30:50 +0200

    zmiana formatu logo

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -15,7 +15,7 @@
 		<div class="hero-head">			
 			<Navigation {segment}/>
 		</div>
-		<img alt="Gazetka firmowa - logo" src="gazetkafirmowa-logo.png">
+		<img alt="Gazetka firmowa - logo" src="gazetkafirmowa-logo.jpg">
 	</div>
 	</section>
 
