94baf89a28fdd165fbc7a64c821a26f6b427144d
Jarosław Wasiak <jaroslaw@wasiak.biz>
2021-12-03 13:58:06 +0100

    zmiana cennika

--- src/routes/cennik.svelte
+++ src/routes/cennik.svelte
@@ -27,44 +27,38 @@
 			<tbody>
 				<tr>
 					<td>Powiat Krosno</td>
-					<td>70,00 zł</td>
-					<td>86,10 zł</td>
+					<td>79,00 zł</td>
+					<td>97,17 zł</td>
 				</tr>
 
 				<tr>
 					<td>Powiat Jasło</td>
-					<td>59,00 zł</td>
-					<td>72,57 zł</td>
+					<td>65,00 zł</td>
+					<td>79,95 zł</td>
 				</tr>
 
 				<tr>
 					<td>Powiat Brzozów</td>
-					<td>55,00 zł</td>
-					<td>67,65 zł</td>
+					<td>59,00 zł</td>
+					<td>72,57 zł</td>
 				</tr>
 
 				<tr>
 					<td>Powiat Sanok</td>
-					<td>59,00 zł</td>
-					<td>72,57 zł</td>
+					<td>65,00 zł</td>
+					<td>79,95 zł</td>
 				</tr>
 
 				<tr>
 					<td>Powiat Gorlice</td>
-					<td>59,00 zł</td>
-					<td>72,57 zł</td>
+					<td>65,00 zł</td>
+					<td>79,95 zł</td>
 				</tr>
 
 				<tr>
 					<td>Powiat Rzeszów</td>
-					<td>85,00 zł</td>
-					<td>104,55 zł</td>
-				</tr>
-
-				<tr>
-					<td>Powiat Nowy Sącz</td>
-					<td>79,00 zł</td>
-					<td>97,17 zł</td>
+					<td>89,00 zł</td>
+					<td>109,47 zł</td>
 				</tr>
 			</tbody>
 		</table>
