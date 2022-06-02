fb7a05e3a4e4baad8a36aa4bd1a0c80a25ab1627
Jarosław Wasiak <jaroslaw@wasiak.biz>
2022-06-02 11:31:45 +0000

    zmiana cen

--- src/routes/cennik.svelte
+++ src/routes/cennik.svelte
@@ -1,68 +1,66 @@
 <svelte:head>
-	<title>Cennik</title>
+    <title>Cennik</title>
 </svelte:head>
 
-
 <section class="section">
-	
-	<div class="container">
-		
-		<h1 class="title">Cennik</h1>
+    <div class="container">
+        <h1 class="title">Cennik</h1>
 
-		<table class="table is-fullwidth is-striped is-size-5">
-			<thead>
-				<tr>
-					<th>Zasięg</th>
-					<th>Cena netto</th>
-					<th>Cena brutto</th>
-				</tr>
-			</thead>
-			
-			<tfoot>
-				<tr >
-				  	<td colspan="3" class="is-size-7 has-text-link">Prezentowane ceny dotyczą jednego modułu. Na pierwszej stronie (okładka) cena jest o 25% wyższa.</td>
-				</tr>
-			  </tfoot>
+        <table class="table is-fullwidth is-striped is-size-5">
+            <thead>
+                <tr>
+                    <th>Zasięg</th>
+                    <th>Cena netto</th>
+                    <th>Cena brutto</th>
+                </tr>
+            </thead>
 
-			<tbody>
-				<tr>
-					<td>Powiat Krosno</td>
-					<td>79,00 zł</td>
-					<td>97,17 zł</td>
-				</tr>
+            <tfoot>
+                <tr>
+                    <td colspan="3" class="is-size-7 has-text-link"
+                        >Prezentowane ceny dotyczą jednego modułu. Na pierwszej stronie (okładka) cena jest o 25%
+                        wyższa.</td
+                    >
+                </tr>
+            </tfoot>
 
-				<tr>
-					<td>Powiat Jasło</td>
-					<td>65,00 zł</td>
-					<td>79,95 zł</td>
-				</tr>
+            <tbody>
+                <tr>
+                    <td>Powiat Krosno</td>
+                    <td>89,00 zł</td>
+                    <td>109,47 zł</td>
+                </tr>
 
-				<tr>
-					<td>Powiat Brzozów</td>
-					<td>59,00 zł</td>
-					<td>72,57 zł</td>
-				</tr>
+                <tr>
+                    <td>Powiat Jasło</td>
+                    <td>79,00 zł</td>
+                    <td>97,17 zł</td>
+                </tr>
 
-				<tr>
-					<td>Powiat Sanok</td>
-					<td>65,00 zł</td>
-					<td>79,95 zł</td>
-				</tr>
+                <tr>
+                    <td>Powiat Brzozów</td>
+                    <td>63,00 zł</td>
+                    <td>77,49 zł</td>
+                </tr>
 
-				<tr>
-					<td>Powiat Gorlice</td>
-					<td>65,00 zł</td>
-					<td>79,95 zł</td>
-				</tr>
+                <tr>
+                    <td>Powiat Sanok</td>
+                    <td>79,00 zł</td>
+                    <td>97,17 zł</td>
+                </tr>
 
-				<tr>
-					<td>Powiat Rzeszów</td>
-					<td>89,00 zł</td>
-					<td>109,47 zł</td>
-				</tr>
-			</tbody>
-		</table>
-		
-	</div>
-	
+                <tr>
+                    <td>Powiat Gorlice</td>
+                    <td>79,00 zł</td>
+                    <td>97,17 zł</td>
+                </tr>
+
+                <tr>
+                    <td>Powiat Rzeszów</td>
+                    <td>95,00 zł</td>
+                    <td>116,85 zł</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </section>
