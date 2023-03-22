fff32a659a49b1121f18618c5a7e38c84f8ad195
Jarosław Wasiak <jaroslaw@wasiak.biz>
2023-03-22 15:51:24 +0000

    zmiana cen

--- src/routes/cennik.svelte
+++ src/routes/cennik.svelte
@@ -5,7 +5,10 @@
 <section class="section">
     <div class="container">
         <h1 class="title">Cennik</h1>
-
+        <p class="is-size-5 m-5" style="text-align: center;">
+            Cena podana poniżej dotyczy wersji <b>podstawowej 1 modułowej</b>. <br />Jest to <b>jednorazowa</b> opłata za
+            okres jednego wydania (3 miesiące)
+        </p>
         <table class="table is-fullwidth is-striped is-size-5">
             <thead>
                 <tr>
@@ -33,26 +36,26 @@
 
                 <tr>
                     <td>Powiat Jasło</td>
-                    <td>79,00 zł</td>
-                    <td>97,17 zł</td>
+                    <td>89,00 zł</td>
+                    <td>109,47 zł</td>
                 </tr>
 
                 <tr>
                     <td>Powiat Brzozów</td>
-                    <td>63,00 zł</td>
-                    <td>77,49 zł</td>
+                    <td>73,00 zł</td>
+                    <td>89,79 zł</td>
                 </tr>
 
                 <tr>
                     <td>Powiat Sanok</td>
-                    <td>79,00 zł</td>
-                    <td>97,17 zł</td>
+                    <td>89,00 zł</td>
+                    <td>109,47 zł</td>
                 </tr>
 
                 <tr>
                     <td>Powiat Gorlice</td>
-                    <td>79,00 zł</td>
-                    <td>97,17 zł</td>
+                    <td>89,00 zł</td>
+                    <td>109,47 zł</td>
                 </tr>
 
                 <tr>
