8c64971f82791d206854d8e36d73c6aa69da9f92
Jarosław Wasiak <jaroslaw@wasiak.biz>
2023-09-20 11:13:48 +0200

    zmiana cennika

--- src/routes/cennik.svelte
+++ src/routes/cennik.svelte
@@ -1,69 +1,69 @@
 <svelte:head>
-    <title>Cennik</title>
+  <title>Cennik</title>
 </svelte:head>
 
 <section class="section">
-    <div class="container">
-        <h1 class="title">Cennik</h1>
-        <p class="is-size-5 m-5" style="text-align: center;">
-            Cena podana poniżej dotyczy wersji <b>podstawowej 1 modułowej</b>. <br />Jest to <b>jednorazowa</b> opłata za
-            okres jednego wydania (3 miesiące)
-        </p>
-        <table class="table is-fullwidth is-striped is-size-5">
-            <thead>
-                <tr>
-                    <th>Zasięg</th>
-                    <th>Cena netto</th>
-                    <th>Cena brutto</th>
-                </tr>
-            </thead>
+  <div class="container">
+    <h1 class="title">Cennik</h1>
+    <p class="is-size-5 m-5" style="text-align: center;">
+      Cena podana poniżej dotyczy wersji <b>podstawowej 1 modułowej</b>.
+      <br />Jest to <b>jednorazowa</b> opłata za okres jednego wydania (3 miesiące)
+    </p>
+    <table class="table is-fullwidth is-striped is-size-5">
+      <thead>
+        <tr>
+          <th>Zasięg</th>
+          <th>Cena netto</th>
+          <th>Cena brutto</th>
+        </tr>
+      </thead>
 
-            <tfoot>
-                <tr>
-                    <td colspan="3" class="is-size-7 has-text-link"
-                        >Prezentowane ceny dotyczą jednego modułu. Na pierwszej stronie (okładka) cena jest o 25%
-                        wyższa.</td
-                    >
-                </tr>
-            </tfoot>
+      <tfoot>
+        <tr>
+          <td colspan="3" class="is-size-7 has-text-link"
+            >Prezentowane ceny dotyczą jednego modułu. Na pierwszej stronie
+            (okładka) cena jest o 25% wyższa.</td
+          >
+        </tr>
+      </tfoot>
 
-            <tbody>
-                <tr>
-                    <td>Powiat Krosno</td>
-                    <td>89,00 zł</td>
-                    <td>109,47 zł</td>
-                </tr>
+      <tbody>
+        <tr>
+          <td>Powiat Krosno</td>
+          <td>99,00 zł</td>
+          <td>121,77 zł</td>
+        </tr>
 
-                <tr>
-                    <td>Powiat Jasło</td>
-                    <td>89,00 zł</td>
-                    <td>109,47 zł</td>
-                </tr>
+        <tr>
+          <td>Powiat Jasło</td>
+          <td>99,00 zł</td>
+          <td>121,77 zł</td>
+        </tr>
 
-                <tr>
-                    <td>Powiat Brzozów</td>
-                    <td>73,00 zł</td>
-                    <td>89,79 zł</td>
-                </tr>
+        <tr>
+          <td>Powiat Brzozów</td>
+          <td>85,00 zł</td>
+          <td>104,55 zł</td>
+        </tr>
 
-                <tr>
-                    <td>Powiat Sanok</td>
-                    <td>89,00 zł</td>
-                    <td>109,47 zł</td>
-                </tr>
+        <tr>
+          <td>Powiat Sanok</td>
+          <td>99,00 zł</td>
+          <td>121,77 zł</td>
+        </tr>
 
-                <tr>
-                    <td>Powiat Gorlice</td>
-                    <td>89,00 zł</td>
-                    <td>109,47 zł</td>
-                </tr>
+        <tr>
+          <td>Powiat Gorlice</td>
+          <td>99,00 zł</td>
+          <td>121,77 zł</td>
+        </tr>
 
-                <tr>
-                    <td>Powiat Rzeszów</td>
-                    <td>95,00 zł</td>
-                    <td>116,85 zł</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+        <tr>
+          <td>Powiat Rzeszów</td>
+          <td>99,00 zł</td>
+          <td>121,77 zł</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </section>
